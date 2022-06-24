75b06a0efac6056fc545e43c836ef8beed28b62e
ali <76276476+mohammad2407@users.noreply.github.com>
2022-06-24 09:44:06 +0530

    building the routes and working on validation of data.

--- Routes/books.js
+++ Routes/books.js
@@ -4,20 +4,25 @@ const router = express.Router();
 const Book = require("../models/Books")
 //Post: creating a new book
 
-router.post('/',(req,res) =>{
+router.post('/', (req, res) => {
+    
     book = new Book({
-        name : req.body.bookName,
-        author:{
-            name:req.body.authorName,
+        name: req.body.bookName,
+        author: {
+            name: req.body.authorName,
             age: req.body.authorAge
         },
         genre: req.body.genre
     });
 
-    book.save().then((book) => {res.send(book)})
-    .catch((error) => {
-        res.status(500).send("Book was not stored in the data base",error)
-    })
+    book
+    .save()
+    .then((book) => {
+         res.send(book) 
+        })
+        .catch((error) => {
+            res.status(500).send("Book was not stored in the data base")
+        })
 
 })
 
--- index.js
+++ index.js
@@ -18,12 +18,15 @@ mongoose.connect(
     process.env.MONGO_URL,
     { useNewUrlParser: true }
 )
-    .then(() => {
+.then(() => {
         console.log("Connected mongo atlas")
     })
   .catch(error => {
     console.log("Some error occured",error)
   })
+
+
+
 app.listen(PORT, () => {
     console.log("port is running at", PORT)
 })
